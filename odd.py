af83516b0d95d5dd847cb0f2d6606d90a0b6dea8
varshanraj <117526984+varshanhack@users.noreply.github.com>
2023-08-03 16:38:37 +0530

    odd.py

--- odd.py
+++ odd.py
@@ -1,14 +1,12 @@
+# Odd or Even
 import sys,subprocess
 while True:
  n=int(input("Enter a number = "))
+ print("")
  if n%2==0:
     print(n,"is even number")
  else:
     print(n,"is odd number")
- print("c = continue\nx = exit")
- x=input("Type c or x ==> ")
- if x=="c":
-     subprocess.run("cls",shell=True)
- else:
-     break
-
+ print("")
+ input("Press any key to continue...")
+ subprocess.run("cls",shell=True)
